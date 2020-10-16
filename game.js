b49f3d43040287f2d91142d6c182fa8599a4254f
Jakub Zlamal <jzlamal@deloittece.com>
2020-10-16 22:34:51 +0200

    root link added

--- game.js
+++ game.js
@@ -133,7 +133,7 @@ getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
     //go to the end page
-    return window.location.assign("/end.html");
+    return window.location.assign("/quiz-app/end.html");
   }
   questionCounter++;
   progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
